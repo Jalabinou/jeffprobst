6fc8bcad61dd18f36246f317ac4b3a526d0dafc2
Jalabinou <45884774+Jalabinou@users.noreply.github.com>
2018-12-15 20:03:33 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,15 +5,8 @@ bot.on("ready", () => {
     bot.user.setPresence({ game: { name: "Survivor",type: 0}})
     console.log("Bot ready !");
     Message("Hey !");
-
 });
 
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.");
-
-    }
-    
 bot.login(process.env.TOKEN);
-})
+
 
