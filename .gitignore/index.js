86b74d216429a45dadd9f9328a5fd7ec208c9071
Jalabinou <45884774+Jalabinou@users.noreply.github.com>
2018-12-15 19:47:23 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.on("ready", () => {
-    bot.user.setPresence({ game: { name: "Ca marche !",type: 0}})
+    bot.user.setPresence({ game: { name: "Survivor",type: 0}})
     console.log("Bot ready !");
 });
 
